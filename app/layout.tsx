dcbf8da5b2477840646383e13c338cc27d7178b0
Gylvaris <bellaantoni21@gmail.com>
2026-01-30 18:56:37 +0100

    refactor: update project metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,14 +2,12 @@ import type { Metadata } from "next";
 import { Geist } from "next/font/google";
 import "./globals.css";
 
-const defaultUrl = process.env.VERCEL_URL
-  ? `https://${process.env.VERCEL_URL}`
-  : "http://localhost:3000";
+const defaultUrl = "http://localhost:3000";
 
 export const metadata: Metadata = {
   metadataBase: new URL(defaultUrl),
-  title: "Next.js and Supabase Starter Kit",
-  description: "The fastest way to build apps with Next.js and Supabase",
+  title: "Finance Manager",
+  description: "The best way to work with your finances",
 };
 
 const geistSans = Geist({
