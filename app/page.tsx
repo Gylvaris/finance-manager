d77c3959aba087fea6bd8d02505285823d33bc10
Gylvaris <bellaantoni21@gmail.com>
2026-01-31 15:24:46 +0100

    refactor: update main page navbar

--- app/page.tsx
+++ app/page.tsx
@@ -4,17 +4,24 @@ export default function Home() {
   return (
     <main className="min-h-screen flex flex-col items-center">
       <div className="flex-1 w-full flex flex-col gap-20 items-center">
-        <nav className="w-full flex justify-center border-b border-b-foreground/10 h-16">
+        <nav className="w-full flex justify-start border-b border-b-foreground/10 h-16">
           <div className="w-full max-w-5xl flex justify-between items-center p-3 px-5 text-sm">
             <div className="flex gap-5 items-center font-semibold">
-              <Link href={"/"}>Next.js Supabase Starter</Link>
+              <Link href={"/"}>Finance Manager</Link>
               <div className="flex items-center gap-2"></div>
+            </div>
+          </div>
+          <div className="w-full max-w-5xl flex justify-end items-center p-3 px-5 text-sm">
+            <div className="flex gap-5 items-center font-semibold">
+              <Link href={"/auth/login"}>Sing In</Link>
             </div>
           </div>
         </nav>
         <div className="flex-1 flex flex-col gap-20 max-w-5xl p-5">
           <main className="flex-1 flex flex-col gap-6 px-4">
-            <h2 className="font-medium text-xl mb-4">Next steps</h2>
+            <h2 className="font-medium text-xl mb-4">
+              Finance Manager (Better name in the future)
+            </h2>
           </main>
         </div>
       </div>
