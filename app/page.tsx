cbd3b520c29ed7396f91620eed4d879c843b55b1
Gylvaris <bellaantoni21@gmail.com>
2026-01-31 15:17:21 +0100

    refactor: remove template footer

--- app/page.tsx
+++ app/page.tsx
@@ -17,20 +17,6 @@ export default function Home() {
             <h2 className="font-medium text-xl mb-4">Next steps</h2>
           </main>
         </div>
-
-        <footer className="w-full flex items-center justify-center border-t mx-auto text-center text-xs gap-8 py-16">
-          <p>
-            Powered by{" "}
-            <a
-              href="https://supabase.com/?utm_source=create-next-app&utm_medium=template&utm_term=nextjs"
-              target="_blank"
-              className="font-bold hover:underline"
-              rel="noreferrer"
-            >
-              Supabase
-            </a>
-          </p>
-        </footer>
       </div>
     </main>
   );
