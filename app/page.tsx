7d89eadb55d247d4d93e8d064453f9046b694db0
Gylvaris <bellaantoni21@gmail.com>
2026-02-01 11:35:57 +0100

    feat: add information about logged user

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,12 @@
 import Link from "next/link";
+import { createClient } from "@/lib/supabase/server";
 
-export default function Home() {
+export default async function Home() {
+  const supabase = await createClient();
+  const {
+    data: { user },
+  } = await supabase.auth.getUser();
+
   return (
     <main className="min-h-screen flex flex-col items-center">
       <div className="flex-1 w-full flex flex-col gap-20 items-center">
@@ -21,6 +27,7 @@ export default function Home() {
           <main className="flex-1 flex flex-col gap-6 px-4">
             <h2 className="font-medium text-xl mb-4">
               Finance Manager (Better name in the future)
+              {user ? <p>Logged in as {user.email}</p> : <p>Not logged in</p>}
             </h2>
           </main>
         </div>
