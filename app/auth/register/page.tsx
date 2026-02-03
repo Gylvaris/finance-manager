b948eeb714a96926b47409d54c9d378526237c83
Gylvaris <bellaantoni21@gmail.com>
2026-02-03 12:18:29 +0100

    feat: add basic registration page

---
+++ app/auth/register/page.tsx
@@ -0,0 +1,20 @@
+export default function RegisterPage() {
+  return (
+    <main>
+      <h1>Register</h1>
+
+      <form action={register}>
+        <input name="email" type="email" placeholder="Email" required />
+
+        <input
+          name="password"
+          type="password"
+          placeholder="Password"
+          required
+        />
+
+        <button type="submit">Create Account</button>
+      </form>
+    </main>
+  );
+}
